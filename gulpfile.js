77c70c6a5785cb733a63fb8e94eee6ea44968e37
Dexin Qi <dexin.qi@hulu.com>
2017-07-23 22:29:37 -0700

    use ' instead of " whenever possible for consistency

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-//var gulp = require("gulp");
-//var ts = require("gulp-typescript");
-//var tsProject = ts.createProject("tsconfig.json");
+//var gulp = require('gulp');
+//var ts = require('gulp-typescript');
+//var tsProject = ts.createProject('tsconfig.json');
 
-//gulp.task("default", function () {
+//gulp.task('default', function () {
     //return tsProject.src()
         //.pipe(tsProject())
-        //.js.pipe(gulp.dest("dist"));
+        //.js.pipe(gulp.dest('dist'));
 //});
 
 //----------------------------------------------------
 
-//var gulp = require("gulp");
-//var browserify = require("browserify");
+//var gulp = require('gulp');
+//var browserify = require('browserify');
 //var source = require('vinyl-source-stream');
-//var tsify = require("tsify");
+//var tsify = require('tsify');
 //var paths = {
     //pages: ['src/*.html']
 //};
 
-//gulp.task("copy-html", function () {
+//gulp.task('copy-html', function () {
     //return gulp.src(paths.pages)
-        //.pipe(gulp.dest("dist"));
+        //.pipe(gulp.dest('dist'));
 //});
 
-//gulp.task("default", ["copy-html"], function () {
+//gulp.task('default', ['copy-html'], function () {
     //return browserify({
         //basedir: '.',
         //debug: true,
@@ -34,20 +34,22 @@
     //.plugin(tsify)
     //.bundle()
     //.pipe(source('bundle.js'))
-    //.pipe(gulp.dest("dist"));
+    //.pipe(gulp.dest('dist'));
 //});
 
 //----------------------------------------------------
 
-var gulp = require("gulp");
-var browserify = require("browserify");
+var gulp = require('gulp');
+var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var watchify = require("watchify");
-var tsify = require("tsify");
-var gutil = require("gulp-util");
+var watchify = require('watchify');
+var tsify = require('tsify');
+var gutil = require('gulp-util');
 var del = require('del');
 var paths = {
-    pages: ['src/*.html']
+    pages: ['src/*.html'],
+    assets: ['assets/*'],
+    phaser: ['bower_components/phaser/build/phaser.js']
 };
 
 var watchedBrowserify = watchify(browserify({
@@ -58,24 +60,34 @@ var watchedBrowserify = watchify(browserify({
     packageCache: {}
 }).plugin(tsify));
 
-gulp.task("copy-html", function () {
+gulp.task('copy-assets', function () {
     return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-html', function () {
+    return gulp.src(paths.assets)
+        .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('copy-libs', function () {
+    return gulp.src(paths.phaser)
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function() {
   return del(['dist/*.js'])
 })
 
-gulp.task("default", ["copy-html", 'clean'], bundle);
+gulp.task('default', ['copy-html', 'clean', 'copy-assets', 'copy-libs'], bundle);
 
 function bundle() {
     return watchedBrowserify
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
 }
 
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+watchedBrowserify.on('update', bundle);
+watchedBrowserify.on('log', gutil.log);
 
